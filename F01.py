63f34a912d538fe715cd1b5ebef77fb03490163e
Ontaaaaaaaa <115411795+Ontaaaaaaaa@users.noreply.github.com>
2023-04-13 10:10:35 +0700

    Update F01.py

--- F01.py
+++ F01.py
@@ -1,12 +1,11 @@
-file = open("user.csv", "r")
-def jadi_array(nama_file, row, line):
-    f = [[None for j in range(line)] for i in range(row)]
+def jadi_array(nama_file, row, collumns):
+    f = [[None for j in range(collumns)] for i in range(row)]
     i = 0
     j = 0
     value = ""
     with open(nama_file, 'r') as file:
-        for line in file:
-            for char in line:  # loop setiap karakter pada baris
+        for collumns in file:
+            for char in collumns:  # loop setiap karakter pada baris
                 if char == ";" or char =='\n':  # jika ditemukan titik koma, maka simpan nilai dalam array f
                     f[i][j] = value
                     value = ""
@@ -19,19 +18,17 @@ def jadi_array(nama_file, row, line):
 
     return f
 
-login = False #Mengetahui status login
-
-f = jadi_array('user.csv',3,3) #Memanggil function yang mengambil file 'user,csv' dan me assign dengan ukuran 3 baris dan 3 kolom atas nama f
-bool = True
-while bool : 
+def login():
+    status = False
+    f = jadi_array('user.csv',3,3) #Memanggil function yang mengambil file 'user,csv' dan me assign dengan ukuran 3 baris dan 3 kolom atas nama f
     username = input("Username: ")  
     password = input("Password: ")
     if username == f[1][0] or username == f[2][0] : #Cek apakah username valid
         if password == f[1][1] or password == f[2][1] : # Cek apakah password valid
             print(f"Selamat datang, {username} !")
-            login = True
-            bool = False
         else :
             print("Password salah!")
     else :
         print("Username tidak terdaftar!")
+    
+    return
