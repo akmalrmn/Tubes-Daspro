a7c6649eeecd077a4dfcbe286544bb5b40e8aae4
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-15 20:33:21 +0700

    Update F01.py

--- F01.py
+++ F01.py
@@ -1,13 +1,13 @@
-def jadi_array(nama_file, row, collumns):
-    f = [[None for j in range(collumns)] for i in range(row)]
+def jadi_array(nama_file, row, collumns): # Fungsi parsing CSV statis
+    CSV_List = [[None for j in range(collumns)] for i in range(row)] # CSV yang sudah diparsing akan dimasukkan ke variabel CSV_List
     i = 0
     j = 0
     value = ""
     with open(nama_file, 'r') as file:
         for collumns in file:
             for char in collumns:  # loop setiap karakter pada baris
-                if char == ";" or char =='\n':  # jika ditemukan titik koma, maka simpan nilai dalam array f
-                    f[i][j] = value
+                if char == ";" or char == '\n':  # jika ditemukan titik koma, maka simpan nilai dalam array f
+                    CSV_List[i][j] = value
                     value = ""
                     j += 1
                     if j >= 3:  # reset kolom jika sudah mencapai batas kolom maksimal
@@ -15,15 +15,15 @@ def jadi_array(nama_file, row, collumns):
                         i += 1
                 else:  # tambahkan karakter ke nilai yang sedang dibaca
                     value += char
-    return f
+    return CSV_List
 
-def login():
-    f = jadi_array("user.csv",101,3) #Memanggil function yang mengambil file 'user,csv' dan me assign dengan ukuran 101 baris dan 3 kolom atas nama f
+def login(): # Fungsi login
+    f = jadi_array("user.csv", 1001, 3) #Memanggil function yang mengambil file 'user.csv' dan melakukan assign dengan ukuran 1001 baris dan 3 kolom atas nama f
     username = input("Username: ")  
     password = input("Password: ")
     i = 1
     login_status = False
-    while i <= 101 and login_status == False :
+    while i <= 1001 and login_status == False :
         if username == f[i][0] :    #Cek apakah username valid
             if password == f[i][1] :    # Cek apakah password valid
                 print(f"Selamat datang, {username}!")
