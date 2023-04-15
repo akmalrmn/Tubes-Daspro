cf681597757b1766e76583cc041171af56d12ea6
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-15 22:55:17 +0700

    Update csvtolist.py

--- csvtolist.py
+++ csvtolist.py
@@ -1,18 +1,23 @@
-def csv_parsing(nama_file, row, collumns): # Fungsi parsing CSV statis
-    CSV_List = [[None for j in range(collumns)] for i in range(row)] # CSV yang sudah diparsing akan dimasukkan ke variabel CSV_List
+def csv_to_list(csv_path, row, collumns):
     i = 0
-    j = 0
-    value = ""
-    with open(nama_file, 'r') as file:
-        for row in file:
-            for char in row:  # loop setiap karakter pada baris
-                if char == ";" or char == '\n':  # jika ditemukan titik koma, maka simpan nilai dalam array f
-                    CSV_List[i][j] = value
-                    value = ""
-                    j += 1
-                    if j >= 3:  # reset kolom jika sudah mencapai batas kolom maksimal
-                        j = 0
-                        i += 1
-                else:  # tambahkan karakter ke nilai yang sedang dibaca
-                    value += char
-    return CSV_List
+    csv_list = [[None for j in range(collumns)] for i in range(row)]
+    with open(csv_path, 'r') as csv:
+        lines = csv.readlines()
+        for baris in lines:
+            kata_grup = []
+            kata = ''
+            for huruf in baris:
+                if huruf == ';':
+                    kata_grup += [kata]
+                    kata = ''
+                elif huruf == '\n':
+                    kata_grup += [kata]
+                    csv_list[i] = kata_grup
+                    i += 1
+                    break
+                else:
+                    kata += huruf
+            else:
+                kata_grup += [kata]
+                csv_list[i] = kata_grup
+    return csv_list
