e41c186a94b778799245cec0fa5748f0ca526614
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-16 15:23:48 +0700

    Update F02_logout.py

--- F02_logout.py
+++ F02_logout.py
@@ -1,13 +1,9 @@
 from F01_login import login
 
-def logout():
-  login_status, i, CSV_List = login()
-  if login_status:
-    print("Logout berhasil!")
-    for j in range(i, 1000):
-      CSV_List[j] = CSV_List[j+1]
-    CSV_List = [row for row in CSV_List if row]
-    print(CSV_List[1])
-  else:
-    print("Logout gagal!")
-    print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout!")
+def logout(login_status, i, CSV_List):
+  print("Logout berhasil!")
+  for j in range(i, 1000):
+    CSV_List[j] = CSV_List[j+1]
+  CSV_List = [row for row in CSV_List if row]
+  login_status = False
+  return CSV_List, login_status
