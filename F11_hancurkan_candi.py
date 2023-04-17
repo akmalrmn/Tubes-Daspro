c6155985a0ce67d8604f3c446ccf8d6f3b732b92
Ontaaaaaaaa <115411795+Ontaaaaaaaa@users.noreply.github.com>
2023-04-17 12:53:14 +0700

    Add files via upload

---
+++ F11_hancurkan_candi.py
@@ -0,0 +1,37 @@
+def hancurkan_candi():
+    import csvtolist
+    import listtocsv
+
+    # Mengubah file candi.csv menjadi array
+    list_candi = csvtolist.csv_to_list('candi.csv',101,5)
+
+    # Input id candi 
+    id_candi = input('Masukan ID candi: ')
+
+    # Cek apakah ada candi dengan id tersebut dan declare atas nama valid
+    valid = False
+    for candi in list_candi:
+        if candi[0] == id_candi:
+            valid = True
+    
+    # Algoritma Hancurkan_candi
+    if not valid :
+        print('Tidak ada candi dengan ID tersebut.')        
+    else :
+        pilihan = input('Apakah anda yakin ingin menghancurkan candi ID: 5 (Y/N)? ')
+        if pilihan == 'N' :
+            print("Candi gagal dihancurkan.")
+        elif pilihan == 'Y' :
+            # cari baris candi dengan ID tersebut
+            loop = True
+            row_num = 0
+            i = 0
+            while loop:
+                if list_candi[i][0] != id_candi:
+                    i += 1
+                    row_num += 1
+                else :
+                    loop = False
+
+    list_candi = list_candi[:row_num] + list_candi[(row_num+1):]
+    listtocsv.list_to_csv(list_candi,'candi.csv')
