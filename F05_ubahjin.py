4c137f9d8de7dbd17cfa865a647eb7c0c876102e
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-19 16:42:27 +0700

    Update and rename F05.py to F05_ubahjin.py

--- F05_ubahjin.py
+++ F05_ubahjin.py
@@ -70,27 +70,30 @@ def count_rows(arr):
     return row_count
 
 
-def ubahjin():
-    if username == 'Bondowoso':
+def ubahjin(csvlist, idx_usn):
+    if csvlist[idx_usn][0] == "Bondowoso":
         username_jin = input('Masukkan username jin : ')
-        for i in range (count_rows(f))   # f = jadi_array('user.csv',baris,kolom) yang bikin zaki:
+        for i in range (count_rows(csvlist)):    
                 #logic: jika username di matriks f ada yang punya peran pembangun atau pengumpul maka dia pasti jin
                 #logic: selain pembangun atau pengumpul maka bukan jin
-                if f[i][0] == username_jin:
-                    if f[i][2] == 'Pengumpul':
+                if csvlist[i][0] == username_jin:
+                    if csvlist[i][2] == 'Pengumpul':
                         jawab = input('Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?')
                         if jawab == 'Y':
-                            f[i][2] == 'Pembangun'
+                            csvlist[i][2] == 'Pembangun'
                             print('Jin telah berhasil diubah.')
-                    elif f[i][2] == 'Pembangun':
+                    elif csvlist[i][2] == 'Pembangun':
                         jawab = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)?')
                         if jawab == 'Y':
-                            f[i][2] == 'Pengumpul'
+                            csvlist[i][2] == 'Pengumpul'
                             print('Jin telah berhasil diubah.')
                     else:
                         #kalau masuk ke else, artinya input username memang ada tapi bukan jin, misal username_jin == 'Bondowoso', 
-                        #maka if f[i][0] = 'Bondowoso', tetapi role bondowoso bukan pengumpul atau pembangun, sehingga dia bukan jin
+                        #maka if csvlist[i][0] = 'Bondowoso', tetapi role bondowoso bukan pengumpul atau pembangun, sehingga dia bukan jin
                         print('Tidak ada jin dengan username tersebut.')
-                #kalau sampai akhir iterasi tidak ditemukan username_jin pada kolom 1 (alias kolom username) pada f, maka
-                elif (i+1) == count_rows(f):
+                #kalau sampai akhir iterasi tidak ditemukan username_jin pada kolom 1 (alias kolom username) pada csvlist, maka
+                elif (i+1) == count_rows(csvlist):
                     print('Tidak ada jin dengan username tersebut.')
+    else:
+      print("Program ini hanya dapat diakses oleh Bondowoso")
+    return csvlist
