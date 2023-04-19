110b8b82a355c0fc9f6e2929391765a91ff8e294
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-19 16:25:00 +0700

    Add files via upload

--- commands.py
+++ commands.py
@@ -1,9 +1,11 @@
 from F01_login import login
 from F02_logout import logout
 from F07_jin_pengumpul import kumpul
+from F03_summonjin import summonjin
+from F04_hilangkanjin import hapus_jin
 
-def run(masukan, login_status, idx_usn, csvlist, idx_rng):
-    if login_status:
+def run(masukan, login_status, idx_usn, csvlist, idx_rng, csvlist2):
+    if login_status: # User sudah melakukan login
         if masukan == "login":
           print("Login gagal!")
           print(f"Anda telah login dengan username {csvlist[idx_usn][0]}, silahkan lakukan “logout” sebelum melakukan login kembali.")
@@ -12,14 +14,18 @@ def run(masukan, login_status, idx_usn, csvlist, idx_rng):
         elif masukan == "kumpul":
           csvlist = kumpul(csvlist, idx_rng)
           print(csvlist)
-        else:
+        elif masukan == "summonjin":
+          csvlist = summonjin(csvlist, idx_usn)
+        elif masukan == "hapusjin":
+          csvlist, csvlist2 = hapus_jin(csvlist, csvlist2, idx_usn)
+        else: # Masukan tidak sesuai dengan opsi yang ada
           print("Opsi tidak tersedia")
-    elif not(login_status):
+    elif not(login_status): # user belum melakukan login
         if masukan == "login":
           login_status, idx_usn, csvlist = login(csvlist)
         elif masukan == "logout":
           print("Logout gagal!")
-          print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout!")
+          print("Anda belum login, silakan login terlebih dahulu sebelum melakukan logout!")
         else:
           print("Opsi tidak tersedia")
-    return login_status, idx_usn, csvlist, idx_rng
+    return login_status, idx_usn, csvlist, idx_rng, csvlist2 # me-return beberapa variabel
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from csvtolist import csv_to_list
 from commands import run
 
-bahan_bangunan = csv_to_list("bahan_bangunan.csv", 4, 3)
+# Mengubah semua file csv ke list in list
+bahan_bangunan = csv_to_list("bahan_bangunan.csv", 4, 3) 
 candi = csv_to_list("candi.csv", 1001, 5)
 user = csv_to_list("user.csv", 1001, 3)
 
-login_status = False
-idx_usn = 1
-idx_rng = 1
+login_status = False # status login
+idx_usn = 1 # Indeks baris dari username saat melakukan login
+idx_rng = 0 # Indeks list random number generator
+csvlist2 = []
 
 for i in range(1, 4):
-  bahan_bangunan[i][2] = 0
+  bahan_bangunan[i][2] = 0 # Melalkukan pengisian list bahan bangunan agar dapat di-assign
 
 while True:
   masukan = input(">>> ")
   if masukan == "kumpul":
-    login_status, idx_usn, bahan_bangunan, idx_rng = run(masukan, login_status, idx_usn, bahan_bangunan, idx_rng)
-    idx_rng += 1
+    login_status, idx_usn, bahan_bangunan, idx_rng, csvlist2 = run(masukan, login_status, idx_usn, bahan_bangunan, idx_rng, csvlist2)
+    idx_rng += 1 # index rng bertambah agar saat dipanggil kembali, akan mengeluarkan number yg berbeda
+  elif masukan == "hapusjin":
+    login_status, idx_usn, user, idx_rng, candi = run(masukan, login_status, idx_usn, user, idx_rng, candi)
   else:
-    login_status, idx_usn, user,idx_rng = run(masukan, login_status, idx_usn, user, idx_rng)
+    login_status, idx_usn, user, idx_rng, csvlist2 = run(masukan, login_status, idx_usn, user, idx_rng, csvlist2)
+    
