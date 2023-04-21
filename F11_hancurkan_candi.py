e603244c6edc56712072f7c8a59615ed8eb3a4d6
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-21 18:43:32 +0700

    Update F11_hancurkan_candi.py

--- F11_hancurkan_candi.py
+++ F11_hancurkan_candi.py
@@ -1,53 +1,48 @@
-def hancurkan_candi():
-    import csvtolist
-    import listtocsv
+def hancurkan_candi(arr_candi, idx_usn, user):
+    if user[idx_usn][0] == "Bondowoso":
+        # Input id candi 
+        id_candi = input('Masukan ID candi: ')
 
-    # Mengubah file candi.csv menjadi list
-    arr_candi = csvtolist.csv_to_list('candi.csv',101,5)
+        # Cek apakah ada candi dengan ID tersebut dan declare atas nama valid
+        valid = False
+        for candi in arr_candi:
+            if candi[0] == id_candi:
+                valid = True
+        
+        # Algoritma Hancurkan_candi
+        if not valid :
+            print('Tidak ada candi dengan ID tersebut.')        
+        else :
+            pilihan = input(f'Apakah anda yakin ingin menghancurkan candi ID: {id_candi} (Y/N)? ')
+            if pilihan == 'N' :
+                print("Candi gagal dihancurkan.")
+            elif pilihan == 'Y' :
+                # cari baris candi dengan ID tersebut
+                loop = True
+                row_num = 0
+                i = 0
+                while loop:
+                    if arr_candi[i][0] != id_candi:
+                        i += 1
+                        row_num += 1
+                    else :
+                        loop = False
 
-    # Input id candi 
-    id_candi = input('Masukan ID candi: ')
-
-    # Cek apakah ada candi dengan ID tersebut dan declare atas nama valid
-    valid = False
-    for candi in arr_candi:
-        if candi[0] == id_candi:
-            valid = True
-    
-    # Algoritma Hancurkan_candi
-    if not valid :
-        print('Tidak ada candi dengan ID tersebut.')        
-    else :
-        pilihan = input(f'Apakah anda yakin ingin menghancurkan candi ID: {id_candi} (Y/N)? ')
-        if pilihan == 'N' :
-            print("Candi gagal dihancurkan.")
-        elif pilihan == 'Y' :
-            # cari baris candi dengan ID tersebut
-            loop = True
-            row_num = 0
-            i = 0
-            while loop:
-                if arr_candi[i][0] != id_candi:
-                    i += 1
-                    row_num += 1
-                else :
-                    loop = False
-
-            # Membuat array baru tanpa element yang mau dihapus
-            arr_candi_baru = [[None for j in range(5)] for i in range (100)]
-            baris = 0
-            kolom = 0
-            for i in range (101):
+                # Membuat array baru tanpa element yang mau dihapus
+                arr_candi_baru = [[None for j in range(5)] for i in range (100)]
+                baris = 0
                 kolom = 0
-                for j in range (5):
+                for i in range (101):
+                    kolom = 0
+                    for j in range (5):
+                        if i != row_num :
+                            arr_candi_baru[baris][j] = arr_candi[i][j]
+                            kolom += 1
                     if i != row_num :
-                        arr_candi_baru[baris][j] = arr_candi[i][j]
-                        kolom += 1
-                if i != row_num :
-                    baris += 1
-                    
-            
-            print("Candi berhasil dihancurkan")
-
-            # Mengubah list menjadi file candi.csv
-            listtocsv.list_to_csv(arr_candi_baru,'candi.csv')
+                        baris += 1
+                        
+                
+                print("Candi berhasil dihancurkan")
+    else:
+        print("Program ini hanya dapat diakses oleh Bondowoso")
+    return arr_candi_baru
