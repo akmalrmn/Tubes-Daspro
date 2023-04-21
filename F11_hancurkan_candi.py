a9cdcfe206178e9122df72a3020448d4f014cc7c
Ontaaaaaaaa <115411795+Ontaaaaaaaa@users.noreply.github.com>
2023-04-21 17:57:34 +0700

    Update F11_hancurkan_candi.py

--- F11_hancurkan_candi.py
+++ F11_hancurkan_candi.py
@@ -3,14 +3,14 @@ def hancurkan_candi():
     import listtocsv
 
     # Mengubah file candi.csv menjadi list
-    list_candi = csvtolist.csv_to_list('candi.csv',101,5)
+    arr_candi = csvtolist.csv_to_list('candi.csv',101,5)
 
     # Input id candi 
     id_candi = input('Masukan ID candi: ')
 
     # Cek apakah ada candi dengan ID tersebut dan declare atas nama valid
     valid = False
-    for candi in list_candi:
+    for candi in arr_candi:
         if candi[0] == id_candi:
             valid = True
     
@@ -18,7 +18,7 @@ def hancurkan_candi():
     if not valid :
         print('Tidak ada candi dengan ID tersebut.')        
     else :
-        pilihan = input('Apakah anda yakin ingin menghancurkan candi ID: 5 (Y/N)? ')
+        pilihan = input(f'Apakah anda yakin ingin menghancurkan candi ID: {id_candi} (Y/N)? ')
         if pilihan == 'N' :
             print("Candi gagal dihancurkan.")
         elif pilihan == 'Y' :
@@ -27,16 +27,27 @@ def hancurkan_candi():
             row_num = 0
             i = 0
             while loop:
-                if list_candi[i][0] != id_candi:
+                if arr_candi[i][0] != id_candi:
                     i += 1
                     row_num += 1
                 else :
                     loop = False
 
-    # Menghapus elemen pada list dengan ID candi tersebut
-    list_candi = list_candi[:row_num] + list_candi[(row_num+1):]
+            # Membuat array baru tanpa element yang mau dihapus
+            arr_candi_baru = [[None for j in range(5)] for i in range (100)]
+            baris = 0
+            kolom = 0
+            for i in range (101):
+                kolom = 0
+                for j in range (5):
+                    if i != row_num :
+                        arr_candi_baru[baris][j] = arr_candi[i][j]
+                        kolom += 1
+                if i != row_num :
+                    baris += 1
+                    
+            
+            print("Candi berhasil dihancurkan")
 
-    # Mengubah list menjadi file candi.csv
-    listtocsv.list_to_csv(list_candi,'candi.csv')
-    
-    return
+            # Mengubah list menjadi file candi.csv
+            listtocsv.list_to_csv(arr_candi_baru,'candi.csv')
