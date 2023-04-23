d9ec112d0acebcec1f6f2b2cf67843a50140a6b1
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-23 22:21:42 +0700

    Delete F16-exit.py

--- F16-exit.py
+++
@@ -1,9 +0,0 @@
-def exit():
-  masukan = input("pakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)").lower()
-  while masukan != "y" and masukan != "n":
-    masukan = input("pakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)").lower()
-  if masukan == "y":
-    save()
-  else:
-    nosave = True
-  return nosave
