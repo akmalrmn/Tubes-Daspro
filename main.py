1cf5d1856771f6f5da4be15e73043683f9a8936b
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-23 13:22:57 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,17 +10,15 @@ user = csv_to_list("user.csv", 1001, 3)
 login_status = False # status login
 idx_usn = 1 # Indeks baris dari username saat melakukan login
 idx_rng = 0 # Indeks list random number generator
-csvlist2 = []
-csvlist3 = []
 
 for i in range(1, 4):
   bahan_bangunan[i][2] = 0 # Melalkukan pengisian list bahan bangunan agar dapat di-assign
 
-berhenti = False
+berhenti = False # indikator pemberhenti program
 
 while berhenti == False:
   sleep(0.8)
   masukan = input(">>> ")
   sleep(0.5)
-  login_status, idx_usn, user, idx_rng, candi, bahan_bangunan = run(masukan, login_status, idx_usn, user, idx_rng, candi, bahan_bangunan)
+  login_status, idx_usn, user, idx_rng, candi, bahan_bangunan = run(masukan, login_status, idx_usn, user, idx_rng, candi, bahan_bangunan) # memanggil fungsi dari commands.py
   print()
