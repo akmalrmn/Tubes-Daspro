96d576e545dedae377d6bd5ad295375a0d17be4e
akmalrmn <118426250+akmalrmn@users.noreply.github.com>
2023-04-23 16:40:13 +0700

    Update F05_ubahjin.py

--- F05_ubahjin.py
+++ F05_ubahjin.py
@@ -1,99 +1,44 @@
-# def ubahjin():
-#     if username == 'Bondowoso':
-#         username_jin = input('Masukkan username jin : ')
-#         if username_jin == 'Jin1':
-#             jawab = input('Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?')
-#             if jawab == 'Y':
-#                 print('Jin telah berhasil diubah.')
-#         elif username_jin == 'Jin2':
-#             jawab = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?')
-#             if jawab == 'Y':
-#                 print('Jin telah berhasil diubah.')
-#         else:
-#             print('Tidak ada jin dengan username tersebut.')
-
-# username = 'Bondowoso'
-# ubahjin()
-
-# penampung_jenis_jin = [['Jin1','Pengumpul'],['Jin2','Pembangun']]
-
-# def ubahjin():
-#     global penampung_jenis_jin
-#     if username == 'Bondowoso':
-#         username_jin = input('Masukkan username jin : ')
-#         if username_jin == 'Jin1':
-#             jawab = input(f'Jin ini bertipe {penampung_jenis_jin [0][1]}. Yakin ingin mengubah ke tipe {penampung_jenis_jin [1][1]} (Y/N)?')
-#             if jawab == 'Y':
-#                 penampung_jenis_jin [0][1] = penampung_jenis_jin [1][1]
-#                 print('Jin telah berhasil diubah.')
-#         elif username_jin == 'Jin2':
-#             jawab = input(f'Jin ini bertipe {penampung_jenis_jin [1][1]}. Yakin ingin mengubah ke tipe {penampung_jenis_jin [0][1]} (Y/N)?')
-#             if jawab == 'Y':
-#                 penampung_jenis_jin [1][1] = penampung_jenis_jin [0][1]
-#                 print('Jin telah berhasil diubah.')
-#         else:
-#             print('Tidak ada jin dengan username tersebut.')
-
-# username = 'Bondowoso'
-# ubahjin()
-# print(penampung_jenis_jin)
-
-# from jenis_jin import penampung_jenis_jin
-
-# def ubahjin():
-#     global penampung_jenis_jin
-#     if username == 'Bondowoso':
-#         username_jin = input('Masukkan username jin : ')
-#         if username_jin == 'Jin1':
-#             jawab = input(f'Jin ini bertipe {penampung_jenis_jin[0][1]}. Yakin ingin mengubah ke tipe {penampung_jenis_jin[1][1]} (Y/N)?')
-#             if jawab == 'Y':
-#                 penampung_jenis_jin[0][1] = penampung_jenis_jin[1][1]
-#                 save_jenis_jin()
-#                 print('Jin telah berhasil diubah.')
-#         elif username_jin == 'Jin2':
-#             jawab = input(f'Jin ini bertipe {penampung_jenis_jin[1][1]}. Yakin ingin mengubah ke tipe {penampung_jenis_jin[0][1]} (Y/N)?')
-#             if jawab == 'Y':
-#                 penampung_jenis_jin[1][1] = penampung_jenis_jin[0][1]
-#                 save_jenis_jin()
-#                 print('Jin telah berhasil diubah.')
-#         else:
-#             print('Tidak ada jin dengan username tersebut.')
-
-# username = 'Bondowoso'
-# ubahjin()
-
-
-def count_rows(arr):
-    row_count = 0
-    for row in arr:
-        row_count += 1
-    return row_count
-
+from time import sleep
 
 def ubahjin(csvlist, idx_usn):
     if csvlist[idx_usn][0] == "Bondowoso":
         username_jin = input('Masukkan username jin : ')
-        for i in range (count_rows(csvlist)):    
+        for i in range (3, 1001):    
                 #logic: jika username di matriks f ada yang punya peran pembangun atau pengumpul maka dia pasti jin
                 #logic: selain pembangun atau pengumpul maka bukan jin
                 if csvlist[i][0] == username_jin:
                     if csvlist[i][2] == 'Pengumpul':
-                        jawab = input('Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?')
+                        sleep(0.5)
+                        print()
+                        jawab = input('Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ')
                         if jawab == 'Y':
-                            csvlist[i][2] == 'Pembangun'
+                            csvlist[i][2] = 'Pembangun'
+                            sleep(0.5)
                             print('Jin telah berhasil diubah.')
+                            break
+                        else:
+                            sleep(0.5)
+                            print('Jin tidak jadi diubah')
+                            break
                     elif csvlist[i][2] == 'Pembangun':
-                        jawab = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)?')
+                        sleep(0.5)
+                        print()
+                        jawab = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ')
                         if jawab == 'Y':
-                            csvlist[i][2] == 'Pengumpul'
+                            csvlist[i][2] = 'Pengumpul'
+                            sleep(0.5)
                             print('Jin telah berhasil diubah.')
-                    else:
-                        #kalau masuk ke else, artinya input username memang ada tapi bukan jin, misal username_jin == 'Bondowoso', 
-                        #maka if csvlist[i][0] = 'Bondowoso', tetapi role bondowoso bukan pengumpul atau pembangun, sehingga dia bukan jin
-                        print('Tidak ada jin dengan username tersebut.')
+                            break
+                        else:
+                            sleep(0.5)
+                            print('Jin tidak jadi diubah')
+                            break
                 #kalau sampai akhir iterasi tidak ditemukan username_jin pada kolom 1 (alias kolom username) pada csvlist, maka
-                elif (i+1) == count_rows(csvlist):
-                    print('Tidak ada jin dengan username tersebut.')
+                elif (i+1) == 1001:
+                    sleep(0.5)
+                    print(f'Tidak ada jin dengan username {username_jin}.')
+                    break
     else:
-      print("Program ini hanya dapat diakses oleh Bondowoso")
+        sleep(0.5)
+        print("Program ini hanya dapat diakses oleh Bondowoso")
     return csvlist
