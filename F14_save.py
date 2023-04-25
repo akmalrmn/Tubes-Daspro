cc841bf72dbcfc63e27a0e0ee290e1fb5ef23bd0
fnathas <123757604+fnathas@users.noreply.github.com>
2023-04-26 01:32:41 +0700

    Update F14_save.py

--- F14_save.py
+++ F14_save.py
@@ -3,13 +3,19 @@ import listtocsv
 
 def save(user,candi,bahan_bangunan):
     def panjang_array(array):
-        i = 0
-        while True:
-            if array[i][0] == None:
-                break
-            else:
-                i += 1
-        return i
+            i = 0
+            while True:
+                if array[i][0] == None:
+                    break
+                else:
+                    i += 1
+            return i
+
+    def array_bersih(array):
+            arr = [None for i in range (panjang_array(array))]
+            for i in range (panjang_array(array)):
+                arr[i] = array[i]
+            return arr
 
     def panjang_string(string):
         kata = string + "\n"
@@ -19,37 +25,29 @@ def save(user,candi,bahan_bangunan):
             jumlah += 1
             i += 1
         return jumlah
-    
-    def array_bersih(array):
-        arr = [None for i in range (panjang_array(array))]
-        for i in range (panjang_array(array)):
-            arr[i] = array[i]
-        return arr
 
-    
+    print(">>> save\n")
     folder = input("Masukkan nama folder: ")
-    folder = f"save/{folder}"
+    folder = "save/" + folder
     print("\nSaving...\n")
+
     folder += "/"
-    jumlah = 0
-    for i in range(panjang_string(folder)):
-        if folder[i] == "/":
-            jumlah += 1
-    i = 0
-    batas = 0
-    path = ""
-    while batas != jumlah:
+    path = ''
+    nama_folder = ''
+    for i in range (panjang_string(folder)):
+        if i < (panjang_string(folder)-1):
+            nama_folder += folder[i]
         if folder[i] != "/":
             path += folder[i]
-            i += 1
         else:
             if os.path.exists(path) == False:
                 print(f"Membuat folder {path}...")
                 os.mkdir(path)
-            batas += 1
-            path += folder[i]
-            i += 1
+                path += "/"
+                
     if os.path.exists(path) == True:      
-        listtocsv.list_to_csv(array_bersih(user),path,'user.csv')
-        listtocsv.list_to_csv(array_bersih(candi),path,'candi.csv')
-        listtocsv.list_to_csv(array_bersih(bahan_bangunan),path,'bahan_bangunan.csv')
+            listtocsv.list_to_csv(array_bersih(user),path,'user.csv')
+            listtocsv.list_to_csv(array_bersih(candi),path,'candi.csv')
+            listtocsv.list_to_csv(array_bersih(bahan_bangunan),path,'bahan_bangunan.csv')
+
+    print(f"Berhasil Menyimpan data di folder {nama_folder}!")
